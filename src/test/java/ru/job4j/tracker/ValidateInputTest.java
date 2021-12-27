fa80b350e9c2ec28bcde82a847e103ade4c446a1
Efmov Maxim <efimov-m-v@ya.ru>
2021-12-27 08:26:28 +0300

    1

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Input in = new StubInput(new String[]{"one", "1"});
+        Output out = new StubOutput();
+
+        ValidateInput vInput = new ValidateInput(out, in);
+        int selected = vInput.askInt("Enter menu:");
+
+        Assert.assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInputManyTimes() {
+        Input in = new StubInput(new String[]{"1", "2"});
+        Output out = new StubOutput();
+
+        ValidateInput vInput = new ValidateInput(out, in);
+        int selected = vInput.askInt("Enter menu:");
+        Assert.assertThat(selected, is(1));
+
+        selected = vInput.askInt("Enter menu:");
+        Assert.assertThat(selected, is(2));
+    }
+
+    @Test
+    public void whenNegativeInput() {
+        Input in = new StubInput(new String[]{"-1"});
+        Output out = new StubOutput();
+
+        ValidateInput vInput = new ValidateInput(out, in);
+        int selected = vInput.askInt("Enter menu:");
+        Assert.assertThat(selected, is(-1));
+    }
+}
