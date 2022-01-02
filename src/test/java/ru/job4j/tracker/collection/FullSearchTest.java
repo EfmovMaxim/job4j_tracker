a319f2f2797f88e932a7da5d6c05eca9370a2d71
Efmov Maxim <efimov-m-v@ya.ru>
2022-01-03 00:27:59 +0300

    HashSet

---
+++ src/test/java/ru/job4j/tracker/collection/FullSearchTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker.collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FullSearchTest {
+
+    @Test
+    public void extractNumber() {
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("1", "First desc")
+        );
+
+        Assert.assertThat(FullSearch.extractNumber(tasks), is(new HashSet<>(List.of("1", "2"))));
+
+    }
+}
