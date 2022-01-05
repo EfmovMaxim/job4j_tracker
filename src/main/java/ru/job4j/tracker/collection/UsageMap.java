c641f7d96c10928180f2a67053c32d612cc6c6a0
Efmov Maxim <efimov-m-v@ya.ru>
2022-01-05 14:51:14 +0300

    HashMap

--- src/main/java/ru/job4j/tracker/collection/UsageMap.java
+++ src/main/java/ru/job4j/tracker/collection/UsageMap.java
@@ -9,10 +9,12 @@ public class UsageMap {
         HashMap<String, String> account = new HashMap<>();
         account.put("11@ya.ru", "Ivanov Ivan");
         account.put("22@ya.ru", "Petrov Petr");
-
+        account.put("11@ya.ru", "Ivanov Ivan11");
         for (Map.Entry<String, String> entry : account.entrySet()) {
             System.out.println(entry);
         }
+
+        System.out.println(account.get("11@ya.ru"));
 
     }
 }
