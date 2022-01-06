e75426985d361a334be813511a55407dce88a65b
Efmov Maxim <efimov-m-v@ya.ru>
2022-01-06 22:48:37 +0300

    Comparator

---
+++ src/main/java/ru/job4j/tracker/collection/LexSort.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+
+       String[] numLeft = left.split(". ", 2);
+       String[] numRight = right.split(". ", 2);
+
+       int num1 = Integer.parseInt(numLeft[0]);
+       int num2 = Integer.parseInt(numRight[0]);
+
+       return Integer.compare(num1, num2);
+    }
+}
