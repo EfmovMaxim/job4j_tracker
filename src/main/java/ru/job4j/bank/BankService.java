5b12ac3bc960b2ea74b51f60b747370ec67af587
Efmov Maxim <efimov-m-v@ya.ru>
2022-01-18 22:45:23 +0300

    Stream API

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -27,23 +27,36 @@ public class BankService {
     }
 
     public User findByPassport(String passport) {
-        for (User user : users.keySet()) {
-            if (passport.equals(user.getPassport())) {
-                return user;
-            }
-        }
-        return null;
+//        for (User user : users.keySet()) {
+//            if (passport.equals(user.getPassport())) {
+//                return user;
+//            }
+//        }
+//        return null;
+        return users.keySet()
+                .stream()
+                .filter(p -> p.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
     }
 
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
+//        if (user != null) {
+//            List<Account> userAccounts = users.get(user);
+//            for (Account account : userAccounts) {
+//                if (requisite.equals(account.getRequisite())) {
+//                    return account;
+//                }
+//            }
+//        }
+
         if (user != null) {
-            List<Account> userAccounts = users.get(user);
-            for (Account account : userAccounts) {
-                if (requisite.equals(account.getRequisite())) {
-                    return account;
-                }
-            }
+            return users.get(user)
+                    .stream()
+                    .filter(acc -> requisite.equals(acc.getRequisite()))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
